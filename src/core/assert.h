ba2574449c0861833e12357220549c7b5c6e2c31
Stewart Dryden Smith <stewart.dryden.smith@gmail.com>
2019-03-13 21:19:42 -0700

    Actually fixes #1. Thanks VS for have diff staging behaviour than CLI.

--- src/core/assert.h
+++ src/core/assert.h
@@ -8,14 +8,18 @@
 namespace Rtek
 {
     extern std::atomic<s32> g_assert_depth;
+    static constexpr s32 s_max_assert_depth = 3;
 }
 
 #ifdef NDEBUG
 #   define RTK_ASSERT(Condition)
 #else
-#   define RTK_ASSERT(Condition)                   \
-        if (!(Condition)) {                        \
-            LOG_ERROR("assert failed: %s\n", #Condition); \
-            abort();                               \
+#   define RTK_ASSERT(Condition)                                          \
+        if (!(Condition)) {                                               \
+            ++g_assert_depth;                                             \
+            if (g_assert_depth <= s_max_assert_depth) {                   \
+                LOG_ERROR("assert failed: %s\n", #Condition);             \
+            }                                                             \
+            abort();                                                      \
         }
 #endif
