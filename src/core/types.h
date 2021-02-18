e49d37e87f43a87b41dcebcfd6aebd7d888bfba6
Stewart Dryden Smith <stewart.dryden.smith@gmail.com>
2021-02-18 01:38:07 -0500

    merge

--- src/core/types.h
+++ src/core/types.h
@@ -1,19 +1,19 @@
 #pragma once
 
-#include <stdint.h>
+#include <cstdint>
 
 namespace Rtek
 {
     using s8  = int8_t;
     using s16 = int16_t;
     using s32 = int32_t;
-    using s32f = int_fast32_t;
+    using s32f = int_fast32_t; /** At least 32bit, larger if faster */
     using s64 = int64_t;
 
     using u8  = uint8_t;
     using u16 = uint16_t;
     using u32 = uint32_t;
-    using u32f = uint_fast32_t;
+    using u32f = uint_fast32_t; /** At least 32bit, larger if faster */
     using u64 = uint64_t;
 
     using f32 = float;
