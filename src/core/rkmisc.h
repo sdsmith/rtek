936e9a8d9943c2f589c785910b0c869a650bdb76
Stewart Dryden Smith <stewart.dryden.smith@gmail.com>
2021-02-18 23:51:02 -0500

    Add type_pun, a safe type punning mechanism

---
+++ src/core/rkmisc.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "core/utility/assert.h"
+
+#include <cstring>
+#include <type_traits>
+
+namespace Rtek
+{
+
+/**
+ * \brief Perform type punning without UB.
+ *
+ * Usage: `float a = 0; int b = type_pun<int>(a);`
+ * ref: https://www.youtube.com/watch?v=sCjZuvtJd-k
+ */
+template<typename ToT, typename FromT>
+ToT type_pun(FromT a) {
+    STATIC_ASSERT(std::is_trivially_constructible_v<FromT>);
+    STATIC_ASSERT(std::is_trivially_constructible_v<ToT>);
+    STATIC_ASSERT_MSG(sizeof(FromT) >= sizeof(ToT),
+        "destination type too small to hold source type");
+
+    ToT ret{};
+    std::memcpy(&ret, &a, sizeof(ret));
+    return ret;
+}
+
+} // namespace Rtek
