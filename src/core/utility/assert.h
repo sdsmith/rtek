ad44f8112652c919c0cd135da2a1351b56402ed7
Stewart Dryden Smith <stewart.dryden.smith@gmail.com>
2021-02-18 23:50:35 -0500

    Add portable STATIC_ASSERT

--- src/core/utility/assert.h
+++ src/core/utility/assert.h
@@ -27,18 +27,55 @@ namespace Rtek
         debug_break();                                                  \
     }
 
-     /**
-      * \def RK_ASSERT(expr)
-      * \brief Assertion.
-      *
-      * Only compiled in when \a ASSERTIONS_ENABLED is set.
-      *
-      * \param expr Expression.
-      */
+/**
+ * \def RK_ASSERT(expr)
+ * \brief Assertion.
+ *
+ * Only compiled in when \a ASSERTIONS_ENABLED is set.
+ *
+ * \param expr Expression.
+ */
 #if ASSERTIONS_ENABLED
 #   define RK_ASSERT(expr) RK_CRITICAL_ASSERT(expr)
 #else
 #   define RK_ASSERT(expr)
 #endif
+
+ /**
+* \def STATIC_ASSERT(expr)
+* \brief Static assertion.
+*
+* \param expr Expression.
+*/
+
+/**
+ * \def STATIC_ASSERT_MSG(expr, msg)
+ * \brief Static assertion with a custom message on failure.
+ *
+ * \param expr Expression.
+ * \param msg Error message.
+ */
+#define _ASSERT_GLUE(a, b) a##b
+#define ASSERT_GLUE(a, b) _ASSERT_GLUE(a, b)
+
+#ifdef RK_CPLUSPLUS
+#   if RK_CPLUSPLUS >= 201103L // >= c++11
+#       define STATIC_ASSERT(expr)                              \
+           static_assert(expr, "static assert failed:" #expr)
+#       define STATIC_ASSERT_MSG(expr, msg) static_assert(expr, msg);
+#   else
+ // no static_assert prior to c++11
+    template <bool> class Static_Assert;
+    template <> class Static_Assert<true> {};
+#       define STATIC_ASSERT(expr)                                      \
+           enum {                                                       \
+               ASSERT_GLUE(g_assert_fail_, __LINE__) = sizeof(Static_Assert<!!(expr)>) \
+           }
+#       define STATIC_ASSERT_MSG(expr, msg) STATIC_ASSERT(expr)
+#   endif
+#endif
+
+#undef ASSERT_GLUE
+#undef _ASSERT_GLUE
 
 } // namespace Rtek
