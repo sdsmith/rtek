eb8e2bbd8526bc0b0e94b1cb0505fe3b77c888f7
Stewart Smith <stewart.dryden.smith@gmail.com>
2021-03-17 20:09:44 -0400

    Add cstring overloads + fix size error
    The windows conversion funcs take the number of wide characters or the
    byte size of the narrow characters.

--- src/core/platform/unicode.h
+++ src/core/platform/unicode.h
@@ -6,7 +6,18 @@
 namespace rk::unicode
 {
 Status wide_to_ansi(std::wstring const& ws, std::string& s) noexcept;
+Status wide_to_ansi(uchar const* ws, std::string& s) noexcept;
+Status wide_to_ansi(uchar const* ws, s32 ws_char_len, std::string& s) noexcept;
+
 Status ansi_to_wide(std::string const& s, std::wstring& ws) noexcept;
+Status ansi_to_wide(char const* s, std::wstring& ws) noexcept;
+Status ansi_to_wide(char const* s, s32 s_byte_size, std::wstring& ws) noexcept;
+
 Status wide_to_utf8(std::wstring const& ws, std::string& s) noexcept;
+Status wide_to_utf8(uchar const* ws, std::string& s) noexcept;
+Status wide_to_utf8(uchar const* ws, s32 ws_char_len, std::string& s) noexcept;
+
 Status utf8_to_wide(std::string const& s, std::wstring& ws) noexcept;
+Status utf8_to_wide(char const* s, std::wstring& ws) noexcept;
+Status utf8_to_wide(char const* s, s32 s_byte_size, std::wstring& ws) noexcept;
 } // namespace rk::unicode
