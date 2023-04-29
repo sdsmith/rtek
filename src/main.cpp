1ee0110630ad3f2b54acfd43d2d71f8277812813
Stewart Smith <stewart.dryden.smith@gmail.com>
2023-04-29 02:25:51 -0700

    Report final status code on error

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,14 @@
 #include "core/utility/no_exception.h"
 #include "core/version.h"
 
+rk::Status rtek_main(rk::unicode::Args& args) {
+    rk::Rtek_Engine engine;
+    RK_CHECK(engine.initialize());
+    RK_CHECK(engine.run());
+    RK_CHECK(engine.destroy());
+    return rk::Status::ok;
+}
+
 int main(int argc, char* argv[])
 {
     // TODO(sdsmith): this arg conversion stuff should really be handled in the engine and not
@@ -14,10 +22,11 @@ int main(int argc, char* argv[])
     // NOTE(sdsmith): Now when referring to argc and argv, they will be UTF-8 encoded
     rk::unicode::Args args(argc, argv); // TODO(sdsmith): throws
 
-    rk::Rtek_Engine engine;
-    RK_CHECK(engine.initialize());
-    RK_CHECK(engine.run());
-    RK_CHECK(engine.destroy());
+    rk::Status ret = rtek_main(args);
+    if (ret != rk::Status::ok) {
+        LOG_ERROR("exited with status: {}", rk::to_string(ret));
+        return 1;
+    }
 
     return 0;
 }
