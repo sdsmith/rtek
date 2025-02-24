8005e29ef0a533ea43bbbbc4b4615144afa4164b
Stewart Dryden Smith <stewart.dryden.smith@gmail.com>
2025-02-23 21:34:30 -0800

    add prototype for resource handle/reference

--- src/core/resman/resman.h
+++ src/core/resman/resman.h
@@ -18,5 +18,23 @@ public:
 
     [[nodiscard]] Status load_texture(std::string const& path, rid& id) noexcept {}
     // load_icon;
+
+    template <typename T>
+    class ResourceRef {
+        /*
+          {
+              Resource_Manager resman;
+              ResourceRef r = resman->load_texture(ENCODE_STR("texture/reference/path"));
+              use(*r);
+          }
+        */
+        Resource() = default;
+        ~Resource() = default;
+        T& operator*() { return resman->get<T>(h); }
+
+    private:
+        Handle<T> h;
+        Resource_Manager* resman = nullptr;
+    };
 };
 } // namespace rk
